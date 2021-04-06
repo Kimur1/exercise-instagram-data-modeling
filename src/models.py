297d22b85bc19ca85e099ea0b9f2d22e49560ca3
Kimur1 <kimur53@gmail.com>
2021-04-06 16:06:40 +0000

    index.html

--- src/models.py
+++ src/models.py
@@ -1,12 +1,24 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String, Enum , UniqueConstraint
+from sqlalchemy import Column, ForeignKey, Integer, String , Enum , UniqueConstraint
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 from eralchemy import render_er
 
 Base = declarative_base()
+
+class Comment(Base):
+    __tablename__ = 'comment'
+    id = column(Integer, primary_key=True)
+    author_id = column(Integer, ForeignKey('user.id'))
+    post_id = column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+    post = relationship(Post)
+
+
+
+
 
 class Follower(Base):
     __tablename__ = 'follower'
@@ -22,33 +34,16 @@ class User(Base):
     firstname = column(String(250), nullable=False)
     lastname = column(String(250), nullable=False)
     email = column(String(250),unique=True, nullable =False)
-    
-class Media(Base):
-    __tablename__ = 'media'
-    id = column(Integer, primary_key=True)
-    typemedia = column(Enum( 'video','photo'), nullable=False)
-    url = column(String(250),nullable=False)
-    post_id = column(Integer, ForeignKey('post.id'), nullable=False)
-    post = relationship(Post)
+   
 
 class Post(Base):
     __tablename__ = 'post'
     id = column(Integer, primary_key=True)
     user_to_id = column(String(250), ForeignKey('user.id'), nullable=False)
     user = relationship(User)
+    url = column(String(250),nullable=False)
+    post_id = column(Integer, ForeignKey('post.id'))
 
-class Comment(Base):
-    __tablename__ = 'comment'
-    id = column(Integer, primary_key=True)
-    author_id = column(Integer, ForeignKey('user.id'))
-    comment_text = column(String(100)), nullable=False
-    post_id = column(Integer, ForeignKey('user.id'))
-    user = relationship(User)
-    post = relationship(Post)
-    
-
-    def to_dict(self):
-        return {}
-
+  
 ## Draw from SQLAlchemy base
-render_er(Base, 'diagram.png')
+render_er(Base,'DIAGRAM.png')
